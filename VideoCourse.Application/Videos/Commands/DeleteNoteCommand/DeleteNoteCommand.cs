5017d28b19b575214847dbd91b211e377d75ef4f
AdiSilagy <92732323+AdiSilagy@users.noreply.github.com>
2022-11-15 08:43:28 +0200

    Making small changes

---
+++ VideoCourse.Application/Videos/Commands/DeleteNoteCommand/DeleteNoteCommand.cs
@@ -0,0 +1,6 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace VideoCourse.Application.Videos.Commands.DeleteNoteCommand;
+
+public record DeleteNoteCommand(Guid Id, Guid VideoId): IRequest<ErrorOr<bool>>;
