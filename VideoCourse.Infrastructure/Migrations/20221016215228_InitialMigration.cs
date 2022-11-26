843898f5874bf2b2985fdc625e683dcbba07db53
AdiSilagy <92732323+AdiSilagy@users.noreply.github.com>
2022-11-26 21:00:11 +0200

    Adding Migrations
    Adding Migrations

---
+++ VideoCourse.Infrastructure/Migrations/20221016215228_InitialMigration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoCourse.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "outbox_messages",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    occurredonutc = table.Column<DateTime>(name: "occurred_on_utc", type: "timestamp with time zone", nullable: false),
+                    publishedonutc = table.Column<DateTime>(name: "published_on_utc", type: "timestamp with time zone", nullable: true),
+                    error = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_outbox_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    firstname = table.Column<string>(name: "first_name", type: "text", nullable: false),
+                    lastname = table.Column<string>(name: "last_name", type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    roleid = table.Column<int>(name: "role_id", type: "integer", nullable: false),
+                    isdeleted = table.Column<bool>(name: "is_deleted", type: "boolean", nullable: false, defaultValue: false),
+                    creationdate = table.Column<DateTime>(name: "creation_date", type: "timestamp with time zone", nullable: false),
+                    updatedate = table.Column<DateTime>(name: "update_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "videos",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    isdeleted = table.Column<bool>(name: "is_deleted", type: "boolean", nullable: false, defaultValue: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    Duration = table.Column<int>(type: "integer", nullable: false),
+                    creatorid = table.Column<Guid>(name: "creator_id", type: "uuid", nullable: false),
+                    ispublished = table.Column<bool>(name: "is_published", type: "boolean", nullable: false, defaultValue: false),
+                    publishedonutc = table.Column<DateTime>(name: "published_on_utc", type: "timestamp with time zone", nullable: true),
+                    creationdate = table.Column<DateTime>(name: "creation_date", type: "timestamp with time zone", nullable: false),
+                    updatedate = table.Column<DateTime>(name: "update_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_videos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_videos_users_creator_id",
+                        column: x => x.creatorid,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sections",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    StartTime = table.Column<int>(type: "integer", nullable: false),
+                    EndTime = table.Column<int>(type: "integer", nullable: false),
+                    videoid = table.Column<Guid>(name: "video_id", type: "uuid", nullable: false),
+                    isdeleted = table.Column<bool>(name: "is_deleted", type: "boolean", nullable: false, defaultValue: false),
+                    creationdate = table.Column<DateTime>(name: "creation_date", type: "timestamp with time zone", nullable: false),
+                    updatedate = table.Column<DateTime>(name: "update_date", type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sections", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_sections_videos_video_id",
+                        column: x => x.videoid,
+                        principalTable: "videos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sections_video_id",
+                table: "sections",
+                column: "video_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_videos_creator_id",
+                table: "videos",
+                column: "creator_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "outbox_messages");
+
+            migrationBuilder.DropTable(
+                name: "sections");
+
+            migrationBuilder.DropTable(
+                name: "videos");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ VideoCourse.Infrastructure/Migrations/20221018195039_QuestionTypeField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoCourse.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuestionTypeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "question_type_id",
+                table: "questions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "question_type_id",
+                table: "questions");
+        }
+    }
+}
